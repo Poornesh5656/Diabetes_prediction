b86759cb29114253b643113c9256461f75eaf557
Poornesh5656 <rajeevamu5656@gmail.com>
2024-12-16 20:40:45 +0530

    Update diabetes_prediction_web_app.py

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model=pickle.load(open('I:/Projects/Stream_lit/trained_model (1).sav','rb'))
+loaded_model=pickle.load(open('models/trained_model (1).sav','rb'))
 
 def diabetes_prediction(input_data):
      
